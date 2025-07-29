ba906815a9e86b19cc82df813252331d75fdf45b
Mehdi LOUZAR <87854904+MehdiLouzar@users.noreply.github.com>
2025-07-29 10:21:30 +0100

    Refactor i18n initialization

--- Front-End/src/components/Providers.tsx
+++ Front-End/src/components/Providers.tsx
@@ -1,17 +1,12 @@
 'use client';
 
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n';
-import '../i18n';
+import { useEffect } from 'react';
+import { initI18n } from '../i18n';
 
-interface ProvidersProps {
-  children: React.ReactNode;
-}
+export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    initI18n();
+  }, []);
 
-export default function Providers({ children }: ProvidersProps) {
-  return (
-    <I18nextProvider i18n={i18n}>
-      {children}
-    </I18nextProvider>
-  );
+  return <>{children}</>;
 }
