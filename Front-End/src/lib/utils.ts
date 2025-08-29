2067465962019ef1acc3b4feda753d4ace284a3c
Youssef EL YAGOUBI <youssef.elyagoubi99@gmail.com>
2025-08-29 17:34:17 +0100

    te

--- Front-End/src/lib/utils.ts
+++ Front-End/src/lib/utils.ts
@@ -26,6 +26,7 @@ export function getBaseUrl() {
   if (typeof window === 'undefined') {
     return process.env.API_INTERNAL_URL || process.env.NEXT_PUBLIC_API_URL || '';
   }
+
   return process.env.NEXT_PUBLIC_API_URL || '';
 }
 
@@ -132,6 +133,8 @@ export async function fetchPublicApi<T>(
   }
   
   const baseUrl = getBaseUrl();
+  console.log("url  =", baseUrl);
+
   const response = await fetch(`${baseUrl}${url}`, {
     ...options,
     headers,
